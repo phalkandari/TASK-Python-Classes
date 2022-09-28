b2163512abfbcc8465da73f500c79c65ae19199a
phalkandari <mohammad.kandary@gmail.com>
2022-09-28 20:46:23 +0300

    Still working on my understanding

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 class Wallet:
-    def __init__(self, money):
-        pass
+    def __init__(self,money):
+        self.money = money
 
-    def credit(amount):
-        pass
+    def credit(self, amount):
+        total_addition = amount + self.money
+        print (f"The new amount of money is {total_addition}")
 
-    def debit(amount):
-        pass
+    def debit(self, amount):
+        self.amount = amount
+        total_subtraction = self.money - self.amount
+        print (f"The new amount of money is {total_subtraction}")
 
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
+wallet = Wallet(0)  # This should default money inside the wallet to 0
 print(wallet)
 
 
-class Person:
-    # Implement the code here
-    pass
+# class Person:
+#     name = "X"
+#     location = "Y"
+#     wallet = "Z"
+
+#     def moveTo(self, point):
+#         self.location = point
+#         print (f"Your new location is {self.location}")
 
 
-person = Person("Moh", 5, 50)
+# person = Person("Moh", 5, 50)
 
 
-class Vendor:
-    # implement Vendor!
-    pass
+# class Vendor:
+#     # implement Vendor!
+#     pass
 
 
-vendor = Vendor("Abdallah", 3, 6)
+# vendor = Vendor("Abdallah", 3, 6)
 
 
-class Customer:
-    # implement Customer!
-    pass
+# class Customer:
+#     # implement Customer!
+#     pass
 
 
-customer = Customer("Abdallah", 3, 6)
+# customer = Customer("Abdallah", 3, 6)
